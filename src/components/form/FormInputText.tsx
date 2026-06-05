4fd2c6285ab23ac0270fa9876f64f9618bb2a0b3
emrahsonmez <emrahsonm3z@gmail.com>
2026-06-06 02:45:09 +0300

    feat(components): app dialog shell and richer form field wrappers

--- src/components/form/FormInputText.tsx
+++ src/components/form/FormInputText.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next'
 import { useField } from 'formik'
 import { InputText } from 'primereact/inputtext'
 
@@ -8,9 +9,11 @@ import { FormField } from './FormField'
 interface FormInputTextProps {
   name: string
   labelKey: TranslationKey
+  placeholderKey?: TranslationKey
 }
 
-export function FormInputText({ name, labelKey }: FormInputTextProps) {
+export function FormInputText({ name, labelKey, placeholderKey }: FormInputTextProps) {
+  const { t } = useTranslation()
   const [field, , helpers] = useField<string>(name)
 
   return (
@@ -19,6 +22,7 @@ export function FormInputText({ name, labelKey }: FormInputTextProps) {
         <InputText
           id={id}
           value={field.value ?? ''}
+          placeholder={placeholderKey ? t(placeholderKey) : undefined}
           invalid={invalid}
           onChange={(event) => {
             void helpers.setValue(event.target.value)
